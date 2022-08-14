954ee0049880c3fa37c9ec1084f2587e9c092501
CoverUp137 <1371462781@qq.com>
2022-08-14 12:14:08 +0800

    first commit

---
+++ ql_update.py
@@ -0,0 +1,49 @@
+import os
+
+
+def writeSha(path, file):
+    cur = os.getcwd()
+    os.chdir(path)
+    result = os.popen('git rev-parse HEAD').read().strip()
+    os.chdir(cur)
+
+    if len(result) == 40 and ' ' not in result:
+        os.system('sed -i "s/commitId/{}/g" {}'.format(result, file))
+        print('已写入 commit id')
+    else:
+        print('commit id 获取失败')
+
+
+if __name__ == "__main__":
+    github_url = 'https://github.com/CoverUp137/wangyiyun.git'
+
+    if os.path.exists('/ql/data/scripts/CoverUp137_wangyiyun'):
+        data_path = '/ql/data/'
+    elif os.path.exists('/ql/scripts/CoverUp137_wangyiyun'):
+        data_path = '/ql/'
+    else:
+        print('未找到路径，请尝试升级青龙')
+        exit(1)
+    
+    scripts_config =  data_path + 'scripts/CoverUp137_wangyiyun/config.json'
+    old_config = data_path + 'scripts/CoverUp137_wangyiyun/config.old.json'
+    example_config = data_path + 'scripts/CoverUp137_wangyiyun/config.example.json'
+    repo_config = data_path + 'repo/CoverUp137_wangyiyun/config.example.json'
+    dependencies = data_path + 'repo/CoverUp137_wangyiyun/requirements.txt'
+    if os.path.exists(scripts_config):
+        print('备份配置文件...')
+        os.system('cp -f {} {}'.format(scripts_config, old_config))
+        print('复制配置示例文件...')
+        os.system('cp -f {} {}'.format(repo_config, example_config))
+        writeSha(os.path.dirname(repo_config), example_config)
+        print('更新配置文件...')
+        os.system('python3 {}scripts/CoverUp137_wangyiyun/updateconfig.py {} {} {}'.format(
+            data_path, example_config, scripts_config, scripts_config))
+        print('更新完成')
+    else:
+        print('复制配置示例文件...')
+        os.system('cp -f {} {}'.format(repo_config, example_config))
+        writeSha(os.path.dirname(repo_config), example_config)
+        print('复制配置文件...')
+        os.system('cp {} {}'.format(example_config, scripts_config))
+
